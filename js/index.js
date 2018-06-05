bde3bc856d4b64ec1dcb31a61a070aae0f12b1da
Jeremiah Strang <jeremiahstrang@jeremiahs-mbp.attlocal.net>
2018-06-05 17:46:28 -0400

    Etc

--- js/index.js
+++ js/index.js
@@ -1,169 +1,182 @@
-$(function() {
-  var batteryPrice = 35000;
-  var destinationAndDocFeePrice = 1000;
-  var longRangeBatteryPrice = 9000;
-  var allWheelDrivePrice = 5000;
-  var enhancedAutopilotPrice = 5000;
-  var premiumUpgradesPackagePrice = 5000;
-  var battery = 'standard';
-  var wheels = 'aero';
-  var color = 'solid-black';
+var batteryPrice = 35000;
+var destinationAndDocFeePrice = 1000;
+var longRangeBatteryPrice = 9000;
+var allWheelDrivePrice = 5000;
+var enhancedAutopilotPrice = 5000;
+var premiumUpgradesPackagePrice = 5000;
+var battery = 'standard';
+var wheels = 'aero';
+var color = 'solid-black';
 
-  var batteryOptions = [
-    {
-      key: 'standard',
-      description: 'Standard Battery',
-      price: 35000,
-    },
-    {
-      key: 'long-range',
-      description: 'Long-Range Battery',
-      price: 44000,
-    },
-    {
-      key: 'performance',
-      description: 'Performance',
-      price: 78000,
-    },
-  ];
+var batteryOptions = [
+  {
+    key: 'standard',
+    description: 'Standard Battery',
+    price: 35000,
+  },
+  {
+    key: 'long-range',
+    description: 'Long-Range Battery',
+    price: 44000,
+  },
+  {
+    key: 'performance',
+    description: 'Performance',
+    price: 78000,
+  },
+];
 
-  var batteryOptionMap = {};
-  for (var i = 0; i < batteryOptions.length; i++) {
-    var option = batteryOptions[i];
-    batteryOptionMap[option.key] = option;
+var batteryOptionMap = {};
+for (var i = 0; i < batteryOptions.length; i++) {
+  var option = batteryOptions[i];
+  batteryOptionMap[option.key] = option;
+}
+
+var colorOptions = {
+  'solid-black': {
+    description: 'Solid Black',
+    price: 0,
+  },
+  'midnight-silver-metallic': {
+    description: 'Midnight Silver Metallic',
+    price: 1000,
+  },
+  'deep-blue-metallic': {
+    description: 'Deep Blue Metallic',
+    price: 1000,
+  },
+  'silver-metallic': {
+    description: 'Silver Metallic',
+    price: 1000,
+  },
+  'pearl-white': {
+    description: 'Pearl White Multi-Coat',
+    price: 1000,
+  },
+  'red': {
+    description: 'Red Multi-Coat',
+    price: 1000,
+  },
+}
+
+var wheelsOptions = {
+  'aero': {
+    description: '18" Aero',
+    price: 0,
+  },
+  'sport': {
+    description: '19" Sport',
+    price: 1500,
+  },
+}
+
+var options = [
+  {
+    key: 'all-wheel-drive',
+    description: 'All-Wheel Drive',
+    price: 5000,
+  },
+  {
+    key: 'premium-upgrades-package',
+    description: 'Premium Upgrades Package',
+    price: 5000,
+  },
+  {
+    key: 'enhanced-autopilot',
+    description: 'Enhanced Autopilot',
+    price: 5000,
+  },
+  {
+    key: 'full-self-driving',
+    description: 'Full Self-Driving Capability',
+    price: 3000,
+  },
+];
+
+function loadPreview() {
+  var selectedOptions = {
+    battery: battery,
+    wheels: wheels,
+    color: color,
+    options: [],
   }
 
-  var colorOptions = {
-    'solid-black': {
-      description: 'Solid Black',
-      price: 0,
-    },
-    'midnight-silver-metallic': {
-      description: 'Midnight Silver Metallic',
-      price: 1000,
-    },
-    'deep-blue-metallic': {
-      description: 'Deep Blue Metallic',
-      price: 1000,
-    },
-    'silver-metallic': {
-      description: 'Silver Metallic',
-      price: 1000,
-    },
-    'pearl-white': {
-      description: 'Pearl White Multi-Coat',
-      price: 1000,
-    },
-    'red': {
-      description: 'Red Multi-Coat',
-      price: 1000,
-    },
+  if (battery === 'performance') {
+    $('#all-wheel-drive-chk, #premium-upgrades-package-chk').prop('checked', true);
+    wheels = 'sport';
   }
 
-  var wheelsOptions = {
-    'aero': {
-      description: '18" Aero',
-      price: 0,
-    },
-    'sport': {
-      description: '19" Sport',
-      price: 1500,
-    },
+  if (!$('#enhanced-autopilot-chk').prop('checked')) {
+    $('#full-self-driving-chk').prop('checked', false);
   }
 
-  var options = [
-    {
-      key: 'all-wheel-drive',
-      description: 'All-Wheel Drive',
-      price: 5000,
-    },
-    {
-      key: 'premium-upgrades-package',
-      description: 'Premium Upgrades Package',
-      price: 5000,
-    },
-    {
-      key: 'enhanced-autopilot',
-      description: 'Enhanced Autopilot',
-      price: 5000,
-    },
-    {
-      key: 'full-self-driving',
-      description: 'Full Self-Driving Capability',
-      price: 3000,
-    },
-  ];
+  $('.battery-option, .color-option, .wheels-option').removeClass('selected');
+  $('.battery-option input[type=checkbox]').prop('checked', false);
+  $('#' + battery).addClass('selected');
+  $('#' + color).addClass('selected');
+  $('#' + wheels).addClass('selected');
+  $('#' + battery + '-chk').prop('checked', true);
+  $('#car-preview').attr('src', 'img/' + color + '-' + wheels + '.png');
 
-  function loadPreview() {
-    if (battery === 'performance') {
-      $('#all-wheel-drive-chk, #premium-upgrades-package-chk').prop('checked', true);
-      wheels = 'sport';
-    }
+  var batteryOption = batteryOptionMap[battery];
 
-    $('.battery-option, .color-option, .wheels-option').removeClass('selected');
-    $('.battery-option input[type=checkbox]').prop('checked', false);
-    $('#' + battery).addClass('selected');
-    $('#' + color).addClass('selected');
-    $('#' + wheels).addClass('selected');
-    $('#' + battery + '-chk').prop('checked', true);
-    $('#car-preview').attr('src', 'img/' + color + '-' + wheels + '.png');
+  var batteryPriceStr = numeral(batteryOption.price).format('$0,0');
+  var destinationAndDocFeePriceStr = numeral(destinationAndDocFeePrice).format('$0,0');
+  var colorPrice = battery !== 'performance' ? colorOptions[color].price : 0;
+  var colorPriceStr = colorPrice !== 0 ? numeral(colorPrice).format('$0,0') : 'Included';
+  var wheelsPrice = battery !== 'performance' ? wheelsOptions[wheels].price : 0;
+  var wheelsPriceStr = wheelsPrice !== 0 ? numeral(wheelsPrice).format('$0,0') : 'Included';
+  var longRangeBatteryPriceStr = numeral(longRangeBatteryPrice).format('$0,0');
+  var allWheelDrivePriceStr = numeral(allWheelDrivePrice).format('$0,0');
+  var enhancedAutopilotPriceStr = numeral(enhancedAutopilotPrice).format('$0,0');
+  var premiumUpgradesPackagePriceStr = numeral(premiumUpgradesPackagePrice).format('$0,0');
 
-    var batteryOption = batteryOptionMap[battery];
+  var totalPrice = batteryOption.price + colorPrice + wheelsPrice + destinationAndDocFeePrice;
 
-
-    var batteryPriceStr = numeral(batteryOption.price).format('$0,0');
-    var destinationAndDocFeePriceStr = numeral(destinationAndDocFeePrice).format('$0,0');
-    var colorPrice = battery !== 'performance' ? colorOptions[color].price : 0;
-    var colorPriceStr = colorPrice !== 0 ? numeral(colorPrice).format('$0,0') : 'Included';
-    var wheelsPrice = battery !== 'performance' ? wheelsOptions[wheels].price : 0;
-    var wheelsPriceStr = wheelsPrice !== 0 ? numeral(wheelsPrice).format('$0,0') : 'Included';
-    var longRangeBatteryPriceStr = numeral(longRangeBatteryPrice).format('$0,0');
-    var allWheelDrivePriceStr = numeral(allWheelDrivePrice).format('$0,0');
-    var enhancedAutopilotPriceStr = numeral(enhancedAutopilotPrice).format('$0,0');
-    var premiumUpgradesPackagePriceStr = numeral(premiumUpgradesPackagePrice).format('$0,0');
-
-    var totalPrice = batteryOption.price + colorPrice + wheelsPrice + destinationAndDocFeePrice;
-
-    for (var i = 0; i < options.length; i++) {
-      var option = options[i];
-      var includeOption = $('#' + option.key + '-chk').prop('checked');
-      if (includeOption) {
-        if (battery === 'performance' &&
-            ['all-wheel-drive', 'premium-upgrades-package', 'long-range-battery'].indexOf(option.key) > -1) {
-          $('#invoice-' + option.key + '-price').html('-');
-        } else {
-          totalPrice += option.price;
-          $('#invoice-' + option.key + '-price').html(numeral(option.price).format('$0,0'));
-        }
-        $('#invoice-' + option.key).show();
+  for (var i = 0; i < options.length; i++) {
+    var option = options[i];
+    var includeOption = $('#' + option.key + '-chk').prop('checked');
+    if (includeOption) {
+      selectedOptions.options.push(option.key);
+      if (battery === 'performance' &&
+        ['all-wheel-drive', 'premium-upgrades-package', 'long-range-battery'].indexOf(option.key) > -1) {
+        $('#invoice-' + option.key + '-price').html('-');
       } else {
-        $('#invoice-' + option.key).hide();
+        totalPrice += option.price;
+        $('#invoice-' + option.key + '-price').html(numeral(option.price).format('$0,0'));
       }
+      $('#invoice-' + option.key).show();
+    } else {
+      selectedOptions.options.splice(i, 1);
+      $('#invoice-' + option.key).hide();
     }
-
-    var totalPriceStr = numeral(totalPrice).format('$0,0');
-
-    $('#invoice-base-description').html('Model 3 - ' + batteryOption.description);
-    $('#invoice-base-price').html(batteryPrice !== 0 ? batteryPriceStr : '-');
-    $('#invoice-destination-and-doc-fee-description').html('Destination and Doc Fee');
-    $('#invoice-destination-and-doc-fee-price').html(destinationAndDocFeePrice !== 0 ? destinationAndDocFeePriceStr : '-');
-    $('#color-description').html(colorOptions[color].description);
-    $('#color-price').html(colorPriceStr);
-    $('#invoice-color-description').html(colorOptions[color].description + ' Paint');
-    $('#invoice-color-price').html(colorPrice !== 0 ? colorPriceStr : '-');
-
-    var wheelsDescription = wheelsOptions[wheels].description;
-    if (battery === 'performance' && wheels === 'sport') {
-      wheelsDescription = '20" Sport';
-    }
-
-    $('#wheels-description').html(wheelsDescription);
-    $('#wheels-price').html(wheelsPriceStr);
-    $('#invoice-wheels-description').html(wheelsDescription + ' Wheels');
-    $('#invoice-wheels-price').html(wheelsPrice !== 0 ? wheelsPriceStr : '-');
-    $('#invoice-total-price').html(totalPriceStr);
   }
 
+  var totalPriceStr = numeral(totalPrice).format('$0,0');
+
+  $('#invoice-base-description').html('Model 3 - ' + batteryOption.description);
+  $('#invoice-base-price').html(batteryPrice !== 0 ? batteryPriceStr : '-');
+  $('#invoice-destination-and-doc-fee-description').html('Destination and Doc Fee');
+  $('#invoice-destination-and-doc-fee-price').html(destinationAndDocFeePrice !== 0 ? destinationAndDocFeePriceStr : '-');
+  $('#color-description').html(colorOptions[color].description);
+  $('#color-price').html(colorPriceStr);
+  $('#invoice-color-description').html(colorOptions[color].description + ' Paint');
+  $('#invoice-color-price').html(colorPrice !== 0 ? colorPriceStr : '-');
+
+  var wheelsDescription = wheelsOptions[wheels].description;
+  if (battery === 'performance' && wheels === 'sport') {
+    wheelsDescription = '20" Sport';
+  }
+
+  $('#wheels-description').html(wheelsDescription);
+  $('#wheels-price').html(wheelsPriceStr);
+  $('#invoice-wheels-description').html(wheelsDescription + ' Wheels');
+  $('#invoice-wheels-price').html(wheelsPrice !== 0 ? wheelsPriceStr : '-');
+  $('#invoice-total-price').html(totalPriceStr);
+  window.location.hash = JSON.stringify(selectedOptions);
+}
+
+$(function() {
   $('.wheels-option').click(function () {
     wheels = $(this).attr('id');
     loadPreview();
@@ -201,6 +214,19 @@ $(function() {
       battery = $(this).attr('id');
       loadPreview();
     }))
+  }
+
+  var windowHash = window.location.hash
+  if (windowHash.length > 1) {
+    var initialOptions = JSON.parse(unescape(windowHash.slice(1, windowHash.length)));
+    if (initialOptions) {
+      battery = initialOptions.battery;
+      color = initialOptions.color;
+      wheels = initialOptions.wheels;
+      for (var i = 0; i < initialOptions.options.length; i++) {
+        $('#' + initialOptions.options[i] + '-chk').prop('checked', true);
+      }
+    }
   }
 
   loadPreview();
